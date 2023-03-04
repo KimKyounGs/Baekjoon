f0c6d91b49040d8cb4b9285a2c51c9757990ce9c
김경일 <77844251+KimKyounGs@users.noreply.github.com>
2023-03-04 09:34:59 +0000

    백준 단계별 풀어보기/ 누적 합 / 구간 합 구하기4 완료

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,11 +1,28 @@
 /*
 # 아이디어 
 
+누적합 설명서 : https://book.acmicpc.net/algorithm/prefix-sum
 */
 #include <iostream>
 
 using namespace std;
 
+int N, M;
+int sum[100001];
+
 int main() {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i ++) {
+        int n;
+        cin >> n;
+        sum[i] = sum[i-1] + n;
+    }
+
+    while(M--) {
+        int i, j;
+        cin >> i >> j;
+        cout << sum[j] - sum[i-1] << '\n'; 
+    }
 }
