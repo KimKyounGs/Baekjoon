49f4bcb176a4521cf7e3754ad34c4978d7fececc
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2023-03-10 15:46:49 +0000

    백준 단계별 풀어보기/ 누적합 문제 완료 단기간 성장 / 백조의 호수 완료

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,56 +1,58 @@
 /*
-# 아이디어 
+아이디어 : 
 
-5 3
-1 2 3 1 2
+23.3.11
 
-sum
-1 3 6 7 9 --> 3개
+어려워서 못 풀었는데 구글에서 검색하고 나니 구간 합 구하기 5(11660) 문제와 동일하게 이차원배열에서의 누적합을 구하는 방식을 써야했었다.
+누적합의 활용 끝판왕까지 본 것 같다.
 
+참고 : https://velog.io/@landlala/c-%EB%B0%B1%EC%A4%80-25682-%EC%B2%B4%EC%8A%A4%ED%8C%90-%EB%8B%A4%EC%8B%9C-%EC%B9%A0%ED%95%98%EA%B8%B0-2
 
-
-
-1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5
-
-
-
-
-sum 
-1. 1 3 6 7 9
-2.   2 5 6 7
-3.     3 4 6
-4.       1 3
-5.         2
-
+문제 접근 : https://jih3508.tistory.com/59
+이차원에서의 누적합 : https://jih3508.tistory.com/50
 
 */
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+int N, M, K;
+char board[2000][2000];
 
-int N, M;
-int result;
-unsigned long long sum[100001];
-
+int chess(char color) {
+	int value, pSum[2001][2001] = {};
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (!((i + j) % 2)) value = board[i][j] != color; // (i + j) % 2 = 0인 경우, 인자로 불러온 color 그대로
+			else value = board[i][j] == color; // (i + j) % 2 = 1인 경우, 인자로 불러온 color 반대로
+			
+			pSum[i + 1][j + 1] = pSum[i][j + 1] + pSum[i + 1][j] - pSum[i][j] + value;
+		}
+	}
+	int tmp, result = 100000000;
+	
+	for (int i = 0; i <= N - K; i++) {
+		for (int j = 0; j <= M - K; j++) {
+			tmp = pSum[i + K][j + K] - pSum[i][j + K] - pSum[i + K][j] + pSum[i][j];
+			result = (tmp < result) ? tmp : result;
+		}
+	}
+	
+	return result;
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> N >> M;
-    for (int i = 1; i <= N; i ++) {
-        int n;
-        cin >> n;
-        sum[i] = sum[i-1] + n;
-        if (sum[i] % M == 0) result ++;
-    }
-
-
-	for (int i = 0; i <= 1000; i++)
-	{
-		result += sum[i] * (sum[i] - 1) / 2;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> N >> M >> K;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> board[i][j];
+		}
 	}
-    
-    cout << result << '\n';
-
+	
+	cout << min(chess('B'), chess('W')) << '\n';
 }
