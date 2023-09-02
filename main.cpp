1ac133a7bc4bc4cf65c7cc23e0ac7b6aede03d5b
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-09-02 06:14:53 +0000

    올리기
    올리기

--- main.cpp
+++ main.cpp
@@ -1,56 +1,48 @@
-/*
-아이디어 :
-
-*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstring>
 
 using namespace std;
 
-int N, K;
-int road[101][2];
-int bike[101][2];
-int dp[101][100001];
-
-int DP(int n, int time) {
-    if (n == 0) return 0;
-    
-    if (dp[n][time] != -1) return dp[n][time];
-
-    dp[n][time] = -1000001;
-
-    if (time - road[n][0] >= 0) {
-        dp[n][time] = max(dp[n][time], DP(n-1, time - road[n][0]) + road[n][1]);
-    }
-
-    if (time - bike[n][0] >= 0) {
-        dp[n][time] = max(dp[n][time], DP(n-1, time - bike[n][0]) + bike[n][1]);
-    }
-
-    return dp[n][time];
-}
+int N, C;
+vector<int> v;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-    cin >> N >> K;
-
-    for (int i = 1; i <= N; i ++) {
-        int a, b, c, d;
-        cin >> a >> b >> c >> d;
-        road[i][0] = a;
-        road[i][1] = b;
-        bike[i][0] = c;
-        bike[i][1] = d;
+    cin >> N >> C;
+    for (int i = 0; i < N; i ++) {
+        int n;
+        cin >> n;
+        v.push_back(n);
     }
 
-    memset(dp, -1, sizeof(dp));
+    sort(v.begin(), v.end());
 
-    cout << DP(N, K) << '\n';
+    int start = 0;
+    int end = v[N-1] - v[0];
 
-	return 0;
+    int result = 0;
+    while(start <= end) {
+        int mid = (start + end) / 2;
+
+        int cnt = 1;
+        int preHouse = v[0];
+        for (int i = 1; i < N; i ++) {
+            if (v[i] - preHouse >= mid) {
+                cnt ++;
+                preHouse = v[i];
+            }
+        }
+
+        if (cnt >= C) {
+            start = mid + 1;
+            result = max(result, mid);
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+
+    cout << result << '\n';
+    
+    
 }
