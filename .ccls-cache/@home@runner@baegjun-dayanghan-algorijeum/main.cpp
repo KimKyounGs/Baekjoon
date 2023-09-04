da2e2fc8becd8ad9c1042b2c6e2ac248ca729780
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-09-04 08:24:44 +0000

    액터 미는중 ?

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -4,45 +4,29 @@
 
 using namespace std;
 
-int N, C;
-vector<int> v;
+string N;
 
 int main() {
-    cin >> N >> C;
-    for (int i = 0; i < N; i ++) {
-        int n;
-        cin >> n;
-        v.push_back(n);
+    while(1) {
+        cin >> N;
+
+        if (N == "0") {
+            break;
+        }
+
+        int start = 0;
+        int end = N.length() - 1;
+        bool check = false;
+        
+        while(start < end) {
+            if (N[start] != N[end]) check = true;
+            start ++;
+            end --;
+        }
+
+        if (check) cout << "no" << '\n';
+        else cout << "yes" << '\n';
     }
-
-    sort(v.begin(), v.end());
-
-    int start = 0;
-    int end = v[N-1] - v[0];
-
-    int result = 0;
-    while(start <= end) {
-        int mid = (start + end) / 2;
-
-        int cnt = 1;
-        int preHouse = v[0];
-        for (int i = 1; i < N; i ++) {
-            if (v[i] - preHouse >= mid) {
-                cnt ++;
-                preHouse = v[i];
-            }
-        }
-
-        if (cnt >= C) {
-            start = mid + 1;
-            result = max(result, mid);
-        }
-        else {
-            end = mid - 1;
-        }
-    }
-
-    cout << result << '\n';
     
-    
+    return 0;
 }
