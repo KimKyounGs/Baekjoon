6adaf5bd515774805cd5283dddfb474e152068c6
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-09-06 05:01:39 +0000

    ++푸는중

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,7 +1,10 @@
 /*
 아이디어 : 
 
+0층부터 i호실에는 i명
+Ex) 1층 -> 3호에는 0층 1호부터 3호까지 합
 
+문제에서 "아래(a-1)층의 1호부터 b호까지 사람들의 수의 합"이라는 문장을 보고 다이나믹 프로그래밍으로 풀면 좋을 것 같았음.
 */
 
 #include <iostream>
@@ -11,59 +14,38 @@
 
 using namespace std;
 
-int N;
-queue<int> q;
+int T;
+int dp[15][15];
 
 int main() {
-    cin >> N;
-    
-    for (int i = 0; i < N; i ++) {
-        string s;
-        cin >> s;
-        
-        if (s == "push") {
-            int n;
-            cin >> n;
+    cin >> T;
 
-            q.push(n);
-        }
-        else if (s == "front") {
-            if (!q.empty()) {
-                cout << q.front() << '\n';
-            }
-            else {
-                cout << -1 << '\n';
-            }
-        }
-        else if (s == "pop") {
-            if (!q.empty()) {
-                cout << q.front() << '\n';
-                q.pop();
-            }
-            else {
-                cout << -1 << '\n';
-            }
-        }
-        else if (s == "size") {
-            cout << q.size() << '\n';
-        }
-        else if (s == "empty") {
-            if (!q.empty()) {
-                cout << 0 << '\n';
-            }
-            else {
-                cout << 1 << '\n';
-            }
-        }
-        else if (s == "back") {
-            if (!q.empty()) {
-                cout << q.back() << '\n';
-            }
-            else {
-                cout << -1 << '\n';
+    for (int i = 0; i <= 14; i ++) {
+        dp[0][i] = i;
+    }
+
+    for (int i = 1; i <= 14; i ++) {
+        for (int j = 1; j <= 14; j ++) {
+            for (int z = 1; z <= j; z ++) {
+                dp[i][j] += dp[i-1][z];
             }
         }
     }
 
+    /*
+    for (int i = 0; i <= 14; i ++) {
+        for (int j = 1; j <= 14; j ++) {
+            cout << dp[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    */
+    
+    while(T--) {
+        int k, n;
+        cin >> k >> n;
+        cout << dp[k][n] << '\n';
+    }
+    
     return 0;
 }
