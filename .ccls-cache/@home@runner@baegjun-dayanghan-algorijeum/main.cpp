f2f0bd7d4a2ea18b146a6b1a3b4992eb49703781
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-09-16 12:36:04 +0000

    열심히 하자!

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,12 +1,6 @@
 /*
 아이디어 : 
 
-0 1 2 3 4 5 6 7 8 9 10 11 12 ... 500000
-
-1. 고장난 번호가 포함된 숫자는 0, 나머지는 1로 초기화 한다.
-2. 1으로 시작해서 + - BFS를 시작한다. --> 변수가 생길 것 같은데 일단 진행.
-
-
 */
 
 #include <iostream>
@@ -19,35 +13,34 @@
 using namespace std;
 
 int N, M;
-vector<int> v;
-int graph[500001];
+int parent[101];
+
+int find_root(int x) {
+    if (x == parent[x]) return x;
+    return parent[x] = find_root(parent[x]);
+}
+
+void union_root(int x, int y) {
+    x = find_root(x);
+    y = find_root(y);
+
+    if (x != y) {
+        parent[y] = x;
+    }
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-    cin >> N;
-    cin >> M;
+    cin >> N >> M;
+
     for (int i = 0; i < M; i ++) {
-        int n;
-        cin >> n;
-        v.push_back(n);
+        int x, y;
+        cin >> x >> y;
     }
 
-    memset(graph, 1, sizeof(graph));
     
-    for (int i = 0; i <= 500000; i ++) {
-        int temp = i;
-        while(i > 0) {
-            for (int j = 0; j < M; j ++) {
-                if (temp % 10 == v[i]) {
-                    graph[i] = 0;
-                    break;
-                }
-            }
-        }
-    }
-
-    for (int i = 0;)
-}
+}    
