002c83eeb07b9ff8fe7f215b1d91776303fbb1e0
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-10-10 10:21:55 +0000

    굿

--- main.cpp
+++ main.cpp
@@ -14,11 +14,22 @@
 
 using namespace std;
 
+int graph[10][10];
+
+void DFS() {
+    
+}
 
 int main() { 
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
     
-
+    for (int i = 0; i < 9; i ++) {
+        for (int j = 0; j < 9; j ++) {
+            scanf("%1d", &graph[i][j]);
+        }
+    }
+    
+    
 }
