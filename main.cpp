5aacfb14feb5383e8ea4beab7a56023ff0bd3ea6
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-10-13 13:21:48 +0000

    백트래킹

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
-/*
-아이디어 :
+ /*
+ 아이디어 :
 
-구간을 3으로 나누기.
-3 3 3
-3 3 3
-3 3 3
+참고
+ https://lu-coding.tistory.com/74
 
-0을 찾으면.
-확인 및 채워넣기.
+ */
 
-참고 : 
-https://yabmoons.tistory.com/129
-
-*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -24,87 +17,47 @@ https://yabmoons.tistory.com/129
 
 using namespace std;
 
-int graph[11][11];
-bool Row[10][10];
-bool Col[10][10];
-bool Square[10][10];
+int N;
+string result;
 
-/*
-bool check(int x, int y, int n) {
-    int endX = 3*(x/3+1);
-    int startX = endX-2;
-    int endY = 3*(y/3+1);
-    int startY = endY-2;
+bool Check(int n) {
+     result.push_back(n+'0');
+     int len = result.length();
 
-    for (int i = 1; i <= 9; i ++) {
-        if (graph[x][i] == n || graph[i][y] == n) {
-            return false;
-        }
-    }
-    
-    for (int i = startX; i <= endX; i ++) {
-        for (int j = startY; j <= endY; j ++) {
-            if (graph[i][j] == n) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-*/
+     for (int i = 1; i <= len/2; i ++) {
+         if (result.substr(len - i, i) == result.substr(len - 2 * i, i)) 
+         {
+             result.pop_back();
+             return false;
+         }
+     }
+     
+     result.pop_back();
+     return true;
+ }
 
-void DFS(int cnt) {
-    if (cnt == 81) {
-        for (int i = 0; i < 9; i ++) {
-            for (int j = 0; j < 9; j ++) {
-                 cout << graph[i][j];
-            }
-            cout << '\n';
-        }
-        exit(0);
-    }
+ void DFS(int cnt) {
+     if (cnt == N) {
+         cout << result << '\n';
+         exit(0);
+     }
 
-    int x = (cnt / 9);
-    int y = (cnt % 9);
+     for (int i = 1; i <= 3; i ++) {
+         if (Check(i)) {
+             result.push_back(i+'0');
+             DFS(cnt + 1);
+             result.pop_back();
+         }
+     }
+ }
 
-    if (graph[x][y] == 0) {
-        for (int i = 1; i <= 9; i ++) {
-            if (Row[x][i] == false && Col[y][i] == false && Square[(x / 3) * 3 + (y / 3)][i] == false)
-            {
-                Row[x][i] = true;
-                Col[y][i] = true;
-                Square[(x / 3) * 3 + (y / 3)][i] = true;
-                graph[x][y] = i;
-                DFS(cnt + 1);
-                graph[x][y] = 0;
-                Row[x][i] = false;
-                Col[y][i] = false;
-                Square[(x / 3) * 3 + (y / 3)][i] = false;
-            }
-        }
-    }
-    else {
-        DFS(cnt + 1);
-    }
-}
 
 int main() { 
     ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    
-    for (int i = 0; i < 9; i ++) {
-        for (int j = 0; j < 9; j ++) {
-            scanf("%1d", &graph[i][j]);
-            if (graph[i][j] != 0)
-            {
-                Row[i][graph[i][j]] = true;
-                Col[j][graph[i][j]] = true;
-                Square[(i / 3) * 3 + (j / 3)][graph[i][j]] = true;
-            }
-        }
-    }
-    //cout << '\n';
-    DFS(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
+     cin >> N;
+
+     DFS(0);
 }
