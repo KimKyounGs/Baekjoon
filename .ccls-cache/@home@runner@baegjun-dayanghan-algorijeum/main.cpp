691e4d02767c97aae27214a21bac13aad78be971
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-01-23 13:21:14 +0000

    삼성기출문제

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -20,28 +20,133 @@ using namespace std;
 
 int N, L;
 int graph[101][101];
-bool visite[101][101];
+bool visit[101][101];
 int result;
 
 void Solution() 
 {
     for (int i = 0; i < N; i ++) {
-        for (int j = 0; j < N; j ++) {
-            if (abs(graph[i][j]-graph[i][j]) == 1) {
-                // 왼쪽에 경사로
-                bool left = false;
-                if (j - L >= 0) {
-                    
+        bool check = true;
+        for (int j = 0; j < N-1; j ++) {
+            if (abs(graph[i][j]-graph[i][j+1]) == 1 && check) {
+                int cnt = 1;
+                // 낮은 칸에 경사로를 놓기. (왼쪽)
+                if (graph[i][j] < graph[i][j+1] && !visit[i][j]) {
+                    visit[i][j] = true;
+                    while(cnt != L) {
+                        if (j - cnt >= 0 && graph[i][j] == graph[i][j-cnt] && !visit[i][j-cnt]) {
+                            visit[i][j-cnt] = true;
+                            cnt ++;
+                        }
+                        else {
+                            check = false;
+                            break;
+                        }
+                    }
                 }
-
-                // 오른쪽에 경사로
-                bool right = false;
-            }
-            else {
+                // 낮은 칸에 경사로를 놓기. (오른쪽)
+                else if (graph[i][j] > graph[i][j+1] && !visit[i][j+1]){
+                    visit[i][j + 1] = true;
+                    while(cnt != L) {
+                        if (j + 1 + cnt <= N-1 && graph[i][j+1] == graph[i][j+1+cnt] && !visit[i][j+1+cnt]) {
+                            visit[i][j+1+cnt] = true;
+                            cnt ++;
+                        }
+                        else {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                else {
+                    check = false;
+                    break;
+                }
+            }   
+            else if (abs(graph[i][j]-graph[i][j+1]) > 1) {
+                check = false;
                 break;
             }
         }
+        if (check == true) {
+            //cout << "가로 = " << i+1 << "번째" << endl;  
+            result ++;
+        }
     }
+
+    /*
+    cout << endl;
+    for (int i = 0; i < N; i++ ) {
+        for (int j = 0; j < N; j ++) {
+            cout << visit[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+    
+
+    memset(visit, 0, sizeof(visit));
+    
+
+    for (int i = 0; i < N; i ++) {
+        bool check = true;
+        for (int j = 0; j < N-1; j ++) {
+            if (abs(graph[j][i]-graph[j+1][i]) == 1 && check) {
+                int cnt = 1;
+                // 낮은 칸에 경사로를 놓기. (왼쪽)
+                if (graph[j][i] < graph[j+1][i] && !visit[j][i]) {
+                    visit[j][i] = true;
+                    while(cnt != L) {
+                        if (j - cnt >= 0 && graph[j][i] == graph[j-cnt][i] && !visit[j-cnt][i]) {
+                            visit[j-cnt][i] = true;
+                            cnt ++;
+                        }
+                        else {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                // 낮은 칸에 경사로를 놓기. (오른쪽)
+                else if (graph[j][i] > graph[j+1][i] && !visit[j+1][i]) {
+                    visit[j+1][i] = true;
+                    while(cnt != L) {
+                        if (j +1 + cnt <= N-1 && graph[j+1][i] == graph[j+1+cnt][i] && !visit[j+1+cnt][i]) {
+                            visit[j+1+cnt][i] = true;
+                            cnt ++;
+                        }
+                        else {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                else {
+                    check = false;
+                    break;
+                }
+            }   
+            else if (abs(graph[j][i]-graph[j+1][i]) > 1) {
+                check = false;
+                break;
+            }
+        }
+        if (check == true) {
+            //cout << "세로 = " << i+1 << "번째" << endl;  
+            result ++;
+        }
+    }
+
+    /*
+    cout << endl;
+    for (int i = 0; i < N; i++ ) {
+        for (int j = 0; j < N; j ++) {
+            cout << visit[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+    
 }
 
 int main()
@@ -54,6 +159,8 @@ int main()
     }
 
     Solution();
+
+    cout << result << endl;
 }
 
 
