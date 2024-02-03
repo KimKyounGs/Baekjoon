d15880596538f217d0ae10eb74bd00a33f0fda26
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-02-03 15:00:40 +0000

    백준 삼성 코테

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,18 +1,6 @@
 /*
 아이디어 :
 
-시간초과로 애먹었던 문제.
-
-처음에는 상어 삭제를 벡터를 지우는 형식으로 했는데 시간 초과가 떠서 shark에 사이즈를 인덱스로 놓고 isLive를 통해 상어가 살아있나 없나를 판단함.
-
-그런데 이제와서 다시해보니
-int time;
-if (shark[i].dir == 1 || shark[i].dir == 2) time = shark[i].speed % ((R-1)*2);
-if (shark[i].dir == 3 || shark[i].dir == 4) time = shark[i].speed % ((C-1)*2);
-
-이 부분이 문제였음.
-사실 벡터로 해도 상관 없었다...
-
 */
 #include <iostream>
 #include <vector>
@@ -28,113 +16,103 @@ if (shark[i].dir == 3 || shark[i].dir == 4) time = shark[i].speed % ((C-1)*2);
 
 using namespace std;
 
-struct Shark {
-    int x;
-    int y;
-    int speed;
-    int dir;
-    int size = 0;
-    bool isLive = false;
+struct Point {
+    int x,y,n;
 };
 
-int R, C, M;
-int graph[101][101];
-int result;
-Shark shark[10001]; // 상어 크기가 인덱스
-int maxIndex;
-int dx[5] = {0, -1, 1, 0, 0};
-int dy[5] = {0, 0, 0, 1, -1};
+int N, M;
+int result = INF;
+int graph[51][51];
+vector<pair<int, int>> v;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
 
-void FishingShark(int col) {
-    for (int i = 1; i <= R; i ++) {
-        if (graph[i][col] != 0) {
-            result += graph[i][col];
-            // 상어 없애기.
-            shark[graph[i][col]].isLive = false;
-            graph[i][col] = 0;
-            break;
-        }
+void BFS() {
+    deque<Point> dq;
+    for (int i = 0; i < M; i ++) {
+        dq.push_back({v[i].first, v[i].second, 3});
+        graph[v[i].first][v[i].second] = 3;
     }
 
-    return;
+    while(!dq.empty()) {
+        int x = dq.front().x;
+        int y = dq.front().y;
+        dq.pop_front();
+        for (int i = 0; i < 4; i ++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N ) continue;
+
+            if (graph[nx][ny] == 0) {
+                dq.push_back({nx,ny,graph[x][y] + 1});
+                graph[nx][ny] = graph[x][y] + 1; 
+            }
+        }
+    }
 }
 
-void MoveShark() {
-    int temp[101][101] = {0,};
-    for (int i = 0; i <= maxIndex; i ++) {
-        if (shark[i].isLive) {
-            int time;
-            if (shark[i].dir == 1 || shark[i].dir == 2) time = shark[i].speed % ((R-1)*2);
-            if (shark[i].dir == 3 || shark[i].dir == 4) time = shark[i].speed % ((C-1)*2);
-            while(time) {
-                int nx = shark[i].x + dx[shark[i].dir];
-                int ny = shark[i].y + dy[shark[i].dir];
+void DFS(int cnt, int idx, int idx2) {
+    if (cnt == M) {
+        int temp[51][51];
+        copy(&graph[0][0], &graph[0][0]+51*51, &temp[0][0]);
 
-                if (nx < 1 || nx > R || ny < 1 || ny > C) {
-                    if (shark[i].dir == 1) shark[i].dir = 2;
-                    else if (shark[i].dir == 2) shark[i].dir = 1;
-                    else if (shark[i].dir == 3) shark[i].dir = 4;
-                    else if (shark[i].dir == 4) shark[i].dir = 3;
-                    continue;
-                }
-
-                shark[i].x = nx;
-                shark[i].y = ny;
-                time --;
-            }
-
-            int x = shark[i].x;
-            int y = shark[i].y;
-
-            // 물고기 자리 배치. (그 자리에 다른 상어가 있으면 크기 비교해서 먹기.)
-            if (temp[x][y] == 0) {
-                temp[x][y] = shark[i].size;
-            } 
-            else {
-                if (temp[x][y] < shark[i].size) {
-                    shark[temp[x][y]].isLive = false;
-                    temp[x][y] = shark[i].size;
-                }
-                else {
-                    shark[i].isLive = false;
+        BFS();
+        
+        bool check = false;
+        for (int i = 0; i < N; i ++) {
+            for (int j = 0; j < N; j ++) {
+                if (graph[i][j] == 0) {
+                    check = true;
                 }
             }
         }
+
+        int maxs = 0;
+        if (!check) {
+            for (int i = 0; i < N; i ++) {
+                for (int j = 0; j < N; j ++) {
+                    if (graph[i][j] >= 4) {
+                        maxs = max(maxs, graph[i][j] - 3);
+                    }
+                }
+            }
+            result = min(result, maxs);
+        }
+        
+        copy(&temp[0][0], &temp[0][0]+51*51, &graph[0][0]);
+        
+        return;
     }
-
-    copy(&temp[0][0], &temp[0][0] + 101*101, &graph[0][0]);
-
+    
+    for (int i = idx; i < N; i ++, idx2 = 0) {
+        for (int j = idx2; j < N; j ++) {
+            if (graph[i][j] == 2) {
+                v.push_back({i,j});
+                DFS(cnt + 1, i, j);
+                v.pop_back();
+            }
+        }
+    }
 }
 
 int main()
 {
-    cin >> R >> C >> M;
-
-    for (int i = 0; i < M; i ++) {
-        int r, c, s, d, z;
-        cin >> r >> c >> s >> d >> z;
-        Shark temp = {r,c,s,d,z,true};
-        shark[z] = temp;
-        graph[r][c] = z;
-        maxIndex = max(maxIndex, z);
-    }
-    
-    for (int i = 1; i <= C; i ++) {
-        FishingShark(i);
-        
-        MoveShark();
-
-        /*
-        cout << "현재 i = " << i << endl;
-        for (int j = 1; j <= R; j ++) {
-            for (int k = 1; k <= C; k ++) {
-                cout << graph[j][k] << ' ';
-            }
-            cout << endl;
+    cin >> N >> M;
+    for (int i = 0; i < N; i ++) {
+        for (int j = 0; j < N; j ++) {
+            cin >> graph[i][j];
         }
-        cout << endl;
-        */
     }
 
-    cout << result << endl;
+    DFS(0, 0, 0);
+
+    if (result == INF) {
+        cout << -1 << '\n';
+    }
+    else {
+        cout << result << endl;
+    }
 }
+
+
