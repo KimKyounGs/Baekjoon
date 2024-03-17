967737605538de8f05019a3e3e4ecaa37f4b9f70
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-03-17 14:53:04 +0000

    삼성 기출

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,10 +1,3 @@
-/*
-아이디어 :
-
-어렵지 않게 풀었던 문제.
-그래프를 다 채우고 마지막에 계산했는데, 더 좋은 방법이 있지 않을까 생각한다..
-
-*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -14,110 +7,167 @@
 #include <cstring>
 #include <deque>
 #include <map>
-#include <climits>
 #define INF 1e9
 #define endl '\n'
 
 using namespace std;
 
+struct Fish {
+    int num, x, y, dir;
+    bool isLive;
+};
+
 int N;
-int graph[401][401];
-vector<int> student[401];
-vector<pair<pair<int, int>, pair<int, int>>> v;
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
+int result = 0;
+vector<Fish> fishs;
+int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
 
-bool compare(pair<pair<int, int>, pair<int, int>> p1, pair<pair<int, int>, pair<int, int>> p2) {
-    if (p1.first.first == p2.first.first) { // 좋아하는 사람 수가 같다면
-        if (p1.first.second == p2.first.second) { // 빈 칸 개수가 같다면 
-            if (p1.second.first == p2.second.first) { // x좌표가 같다면 
-                return p1.second.second < p2.second.second; // y좌표 기준으로 오름차순
-            }
-            return p1.second.first < p2.second.first; // x좌표 기준으로 오름차순
-        }
-        return p1.first.second > p2.first.second; // 빈 칸 개수를 기준으로 내림차순
-    }
-    return p1.first.first > p2.first.first; // 좋아하는 사람 수를 기준으로 내림차순
+bool Compare(const Fish& p1, const Fish& p2) {
+    return p1.num < p2.num;
 }
 
-void Fill(int n, int a, int b, int c, int d) {
-    for (int i = 1; i <= N; i ++) {
-        for (int j = 1; j <= N; j ++) {
-            if (graph[i][j] == 0) {
-                int like = 0;
-                int blank = 0;
-                for (int k = 0; k < 4; k ++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
+void FishMove(int sharkX, int sharkY, int (&graph)[5][5]) {
+    for (int i = 0; i < 16; i ++) {
+        // 살아 있는 물고기만 움직이기.
+        if (fishs[i].isLive == true) {
+            int num = fishs[i].num;
+            int dir = fishs[i].dir;
+            int x = fishs[i].x;
+            int y = fishs[i].y;
 
-                    if (nx < 1 || ny < 1 || nx > N || ny > N) continue;
+            while(true) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
 
-                    if (graph[nx][ny] == a || graph[nx][ny] == b || graph[nx][ny] == c || graph[nx][ny] == d) like ++;
-                    
-                    if (graph[nx][ny] == 0) blank ++;
+                // 경계선 넘어가거나 상어가 있는 칸은 건너뛰기
+                if (nx < 0 || ny < 0 || nx > 3 || ny > 3 || (nx == sharkX && ny == sharkY)) {
+                    dir ++;
+                    if (dir == 8) {
+                        dir = 0;
+                    }
+                    continue;
+                } 
+
+                // 이동할 칸에 다른 물고기가 있다면
+                if (graph[nx][ny] != 0) {
+                    // 이동하는 물고기 정보 업데이트
+                    fishs[i].x = nx;
+                    fishs[i].y = ny;
+                    fishs[i].dir = dir;
+
+                    // 원래 있던 물고기 정보 업데이트
+                    int tempNum = graph[nx][ny];
+                    fishs[tempNum-1].x = x;
+                    fishs[tempNum-1].y = y;
+
+                    // graph 업데이트
+                    graph[x][y] = tempNum;
+                    graph[nx][ny] = num;
                 }
+                // 이동할 칸이 빈칸이면
+                else {
+                    // 이동하는 물고기 정보 업데이트
+                    fishs[i].x = nx;
+                    fishs[i].y = ny;
+                    fishs[i].dir = dir;
 
-                v.push_back({{like,blank},{i,j}});
-            } 
-        }
+                    // graph 업데이트
+                    graph[x][y] = 0;
+                    graph[nx][ny] = num;
+                }
+                break;
+            }
+        } 
     }
 }
 
-int main() 
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+vector<pair<int, int>> SharkMove(int sharkX, int sharkY, int sharkDir, int (&graph)[5][5]) {
+    vector<pair<int, int>> v;
+    int tempX = sharkX, tempY = sharkY;
+    while(true) {
+        int nx = tempX + dx[sharkDir];
+        int ny = tempY + dy[sharkDir];
+        
+        if (nx < 0 || ny < 0 || nx > 3 || ny > 3) break;
 
-    cin >> N;
-    for (int i = 0; i < N*N; i ++) {
-        int n, a, b, c, d;
-        cin >> n >> a >> b >> c >> d;
-        student[i].push_back(n);
-        student[i].push_back(a);
-        student[i].push_back(b);
-        student[i].push_back(c);
-        student[i].push_back(d);
-        
-        Fill(n, a, b, c, d);
-        sort(v.begin(), v.end(), compare);
-        
-        int x = v[0].second.first;
-        int y = v[0].second.second;
-        student[i].push_back(x);
-        student[i].push_back(y);
-        
-        graph[x][y] = n;
+        // 상어가 이동할 좌표에 물고기가 있다면
+        if (graph[nx][ny] != 0) {
+            v.push_back({nx, ny});
+        }
+        tempX = nx;
+        tempY = ny;
+        cout << "Debug = " << tempX << ' ' << tempY << ' ' << sharkDir << endl;
+    }
+    return v;
+}
 
-        v.clear();
+void DFS(int sum, int sharkX, int sharkY, int sharkDir, int (&graph)[5][5]) {
+    int temp[5][5];
+    copy(&temp[0][0], &temp[0][0]+5*5, &graph[0][0]);
+    FishMove(sharkX, sharkY, graph);
+    vector<pair<int, int>> v = SharkMove(sharkX, sharkY, sharkDir, graph);
+    
+    if (v.size() == 0) {
+        result = max(result, sum);
+        return;
     }
 
-    // for (int i = 1; i <= N; i ++) {
-    //     for (int j = 1; j <= N; j ++) {
+    for (int i = 0; i < v.size(); i ++) {
+        int x = v[i].first;
+        int y = v[i].second;
+        int num = graph[x][y];
+        sum += num;
+        graph[x][y] = 0;
+        fishs[num-1].isLive = false;
+        DFS(sum, x, y, fishs[num-1].dir, graph);
+        copy(&graph[0][0], &graph[0][0]+5*5, &temp[0][0]);
+        sum -= num;
+        graph[x][y] = num;
+        fishs[num-1].isLive = true;
+    }
+    
+}
+
+int main()
+{   
+    int sum = 0;
+    int sharkX;
+    int sharkY;
+    int sharkDir;
+    int graph[5][5] = {0,};
+    for (int i = 0; i < 4; i ++) {
+        for (int j = 0; j < 4; j ++) {
+            int a, b;
+            cin >> a >> b;
+            // 첫번째 물고기는 먹는다.
+            if (i == 0 && j == 0) {
+                sharkX = i;
+                sharkY = j;
+                sharkDir = b-1;
+                sum = a;
+                fishs.push_back({a,i,j,b-1,false});
+                continue;
+            }
+            graph[i][j] = a;
+            fishs.push_back({a,i,j,b-1,true});
+        }
+    }
+    // 번호대로 움직이기 위해 정렬
+    sort(fishs.begin(), fishs.end(), Compare);
+
+    DFS(sum, sharkX, sharkY, sharkDir, graph);
+
+    cout << result << endl;
+    // for (int i = 0; i < 4; i ++) {
+    //     for (int j = 0; j < 4; j ++) {
     //         cout << graph[i][j] << ' ';
     //     }
     //     cout << endl;
     // }
 
-    int result = 0;
-    for (int i = 0; i < N*N; i ++) {    
-        int cnt = 0;
-
-        int x = student[i][5];
-        int y = student[i][6];
-
-        for (int j = 0; j < 4; j ++) {
-            int nx = x + dx[j];
-            int ny = y + dy[j];
-
-            if (nx < 1 || ny < 1 || nx > N || ny > N) continue;
-
-            if (graph[nx][ny] == student[i][1] || graph[nx][ny] == student[i][2] || graph[nx][ny] == student[i][3] || graph[nx][ny] == student[i][4]) cnt ++;
-        }
-
-        if (cnt) result += pow(10, cnt-1);
-    }
-
-    cout << result << endl;
-    return 0;
+    // cout << endl;
+    // for (int i = 0; i < fishs.size(); i ++) {
+    //     cout << i + 1 << "번의 물고기 정보" << fishs[i].x << ' ' << fishs[i].y << ' ' << fishs[i].dir << ' ' << fishs[i].isLive << endl;
+    // }
 }
