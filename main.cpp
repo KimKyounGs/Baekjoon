261e909cea7ac775a29cb3c2c3c90734f6241011
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-06-16 15:00:53 +0000

    트리의 지름(1167)

--- main.cpp
+++ main.cpp
@@ -14,37 +14,82 @@
 
 using namespace std;
 
-int N;
-int graph[500][500];
-pair<int, int> TornadoLocation;
-
-void MoveTornado() {
-    
-}
-
-void SpreadSand() {
-    
-}
+int N, k;
+int graph[51][51];
+int result = INF;
+// 동 = 0, 남 = 1, 서 = 2, 북 = 3
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+// 0번 타일은 차 방향이 3(북)일 때 1(동)으로 바뀌고 혹인 서(2)일 때 1(남)으로 바뀜
+pair<pair<int, int>, pair<int, int>> tileDir[] = { {{3,0},{2,1}}, {{0,1},{3,2}}, {{1,0},{2,3}}, {{0,3},{1,2}}, {{1,1},{3,3}}, {{0,0},{2,2}} };
 
 void Simulation() {
-    while(!(TornadoLocation.first == 1 && TornadoLocation.second == 1)) {
-        MoveTornado();
-        SpreadSand();
+    int distance = 1;
+    int carX = 0, carY = 0, carDir = 0;
+    while(1) {
+        int tileType = graph[carX][carY];
+        if (tileDir[tileType].first.first == carDir) {
+            carDir = tileDir[tileType].first.second;
+            carX += dx[carDir];
+            carY += dy[carDir];
+            distance ++;
+            cout << "Debug = type 1 -> " << carX << ' ' << carY << ' ' << carDir  << ' ' << tileType << '\n';
+            if (carX < 0 || carY < 0 || carX > N-1 || carY > N-1) return;
+        }
+        else if (tileDir[tileType].second.first == carDir) {
+            carDir = tileDir[tileType].second.second;
+            carX += dx[carDir];
+            carY += dy[carDir];
+            distance ++;
+            cout << "Debug = type 2 -> " << carX << ' ' << carY << ' ' << carDir << ' ' << tileType << '\n';
+            if (carX < 0 || carY < 0 || carX > N-1 || carY > N-1) return;
+        }
+        else {
+            return;
+        }
+        // cout << carX << ' ' << carY << ' ' << carDir << '\n';
+        if (carX == N-1 && carY == N) break;
+    }
+    if(distance != 0) result = min(result, distance);
+}
+
+void DFS(int cnt, int idx, int idx2) {
+    if (cnt == k) {
+        Simulation();
+        return;
+    }
+
+    for (int i = idx; i < N; i ++, idx2 = 0) {
+        for(int j = idx2; j < N; j ++) {
+            int temp = graph[i][j];
+            for (int k = 0; k <= 5; k ++) {
+                if (temp == k) continue;
+                graph[i][j] = k;
+                DFS(cnt + 1, i, j);
+                graph[i][j] = temp;
+            }
+        }
     }
 }
 
 int main()
 {   
-    cin >> N;
-    TornadoLocation = {N/2+1, N/2+1};
-    for (int i = 1; i <= N; i ++) {
-        for (int j = 1; j <= N; j ++) {
+    cin >> N >> k;
+    for (int i = 0; i < N; i ++) {
+        for (int j = 0; j < N; j ++) {
             cin >> graph[i][j];
         }
     }
 
-    Simulation();
-    
-        
+    // graph[2][1] = 3;
+    Simulation(); // 바꾸지 않아도 될 수 있는 경우
+    // DFS(0, 0, 0);
+
+    if (result == INF) {
+        cout << -1 << '\n';
+    }
+    else {
+        cout << result << '\n';
+    }
     return 0;
 }
