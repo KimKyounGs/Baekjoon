b9245dd5943cb623a762e7503d5b609ee8359b1d
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-10-12 06:02:25 +0000

    동전 1(2293)

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -17,9 +17,40 @@
 
 using namespace std;
 
+int n, k;
+vector<int> v;
+int dp[10001];
+
+// 1 2 3
+// 1 -> 1 
+// 2 -> 1 1 / 2
+// 3 -> 1 1 1 / 2 1 / 3
+// 4 -> 1 1 1 1 / 2 2 / 3 1
+// 5 -> 1 1 1 1 1 / 2 1 1 1, 2 2 1 / 3 2
+
+// 1원으로 만들 수 있는 방법을 생각한 뒤에 1, 2로 만들 수 있는 방법 더헤주고 1,2,3 으로 만들 수 있는 방법 더해주기..
+// 이런 식으로 겹치지 않게 해줌.
+
 int main()
-{
-    int good;
+{    
+    cin >> n >> k;
+    for (int i = 0; i < n; i ++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    dp[0] = 1;
+    int len = v.size();
+    for (int i = 0; i < len; i ++) {
+        for (int j = v[i]; j <= k; j ++) {
+            dp[j] += dp[j-v[i]];
+        }
+    }
+
+    cout << dp[k] << '\n';
 }
 
 
