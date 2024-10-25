8d30ffffc7ee1391db33972ecd77bee7ef201fe7
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-10-25 14:41:55 +0000

    코테 준비

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -17,40 +17,33 @@
 
 using namespace std;
 
-int n, k;
-vector<int> v;
-int dp[10001];
-
-// 1 2 3
-// 1 -> 1 
-// 2 -> 1 1 / 2
-// 3 -> 1 1 1 / 2 1 / 3
-// 4 -> 1 1 1 1 / 2 2 / 3 1
-// 5 -> 1 1 1 1 1 / 2 1 1 1, 2 2 1 / 3 2
-
-// 1원으로 만들 수 있는 방법을 생각한 뒤에 1, 2로 만들 수 있는 방법 더헤주고 1,2,3 으로 만들 수 있는 방법 더해주기..
-// 이런 식으로 겹치지 않게 해줌.
+int T, N;
+int dp[2][100001];
+int arr[2][100001];
 
 int main()
 {    
-    cin >> n >> k;
-    for (int i = 0; i < n; i ++) {
-        int num;
-        cin >> num;
-        v.push_back(num);
-    }
-
-    sort(v.begin(), v.end());
-
-    dp[0] = 1;
-    int len = v.size();
-    for (int i = 0; i < len; i ++) {
-        for (int j = v[i]; j <= k; j ++) {
-            dp[j] += dp[j-v[i]];
+    cin >> T;
+    while(T--) {
+        cin >> N;
+        for (int i = 0; i < 2; i ++) {
+            for (int j = 1; j <= N; j ++) {
+                cin >> arr[i][j];
+            }
         }
-    }
+        // 초기화
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        dp[1][0] = arr[1][0];
+        dp[1][1] = arr[1][1];
+        
+        for (int i = 1; i <= N; i ++) {
+            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+        }
 
-    cout << dp[k] << '\n';
+        cout << max(dp[0][N], dp[1][N]) << endl;
+    }
 }
 
 
