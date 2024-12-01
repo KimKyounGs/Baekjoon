4b04f9ccb49be407db44c3f65f7285a308747b75
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-12-01 14:18:32 +0000

    마지막 문제(28110)

--- .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
+++ .ccls-cache/@home@runner@baegjun-dayanghan-algorijeum/main.cpp
@@ -1,6 +1,6 @@
 /*
 아이디어 :
-설명 : https://velog.io/@jua0610/%EB%B0%B1%EC%A4%80-11657-%ED%83%80%EC%9E%84%EB%A8%B8%EC%8B%A0-%EB%B2%A8%EB%A7%8C-%ED%8F%AC%EB%93%9C-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
+
 */
 #include <iostream>
 #include <vector>
@@ -16,45 +16,41 @@
 
 using namespace std;
 
-int N, M, x, y, z;
-long long dist[501];
-bool cycle;
-
-vector <pair <int, int>> edge[501];
-
-void Bellmanford() {
-    fill(dist, dist + N + 1, INF); // 변경하려는 원소의 범위 시작주소, 변경하려는 원소 개수, 변경 값
-    dist[1] = 0;
-
-    for (int k = 1; k <= N; k++) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j < edge[i].size(); j++) {
-                int next = edge[i][j].first;
-                int nextcost = edge[i][j].second;
-
-                if (dist[i] != INF && dist[next] > dist[i] + nextcost) {
-                    dist[next] = dist[i] + nextcost;
-                    if (k == N) cycle = true;
-                }
-            }
-        }
-    }
-    if (cycle) cout << -1;
-    else {
-        for (int i = 2; i <= N; i++)
-      // dist[i] = INF 경우는 해당 도시로 가는 경로가 없는 것
-            cout << (dist[i] != INF ? dist[i] : -1) << "\n";
-    }
-}
+int N;
+vector<int> v;
 
 int main()
 {
-    cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-        cin >> x >> y >> z;
-        edge[x].push_back({ y, z });
+    cin >> N;
+    for (int i = 0; i < N; i ++) 
+    {
+        int n;
+        cin >> n;
+        v.push_back(n);
     }
-    Bellmanford();
+
+    sort(v.begin(), v.end());
+    int a = 0;
+    int b = 0;
+    int maxs = 0;
+
+    for (int i = 0; i < N-1; i ++) 
+    {
+        if (maxs < v[i+1] - v[i])
+        {
+            a = i+1;
+            b = i;
+            maxs = v[i+1] - v[i];
+        }
+    }
+
+    if (maxs == 1)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << (v[a] + v[b])/2;
+    }
+    
 }
-
-
